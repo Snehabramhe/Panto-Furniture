57d90836209e50ecf05ab877715878ebd103241c
Sneha Bramhe <snehabramhe03@gmail.com>
2026-01-16 23:17:33 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center">
+        Make Your Interior More <br/>
+        Minimalistic & Modern
+      </h1>
+     <p className="text-center">
+      Turn your room with panto into a lot more minimalist <br /> 
+      and modern with ease and speed
+     </p>
+    </>
+  );
+}
+
+export default App;
